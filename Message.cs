2b59fe2b6932b2909eb4c9e0316eb76842e336a9
Winciwizard <vincent.verges@gmail.com>
2018-06-19 11:54:43 +0200

    first commit

---
+++ Message.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Message
+    {
+        //Création des 3 DateTime avec les horaires de délimitages 9h, 13h et 18h
+        DateTime morning = new DateTime(2018, 6, 19, 9, 0, 0);
+        DateTime afternoon = new DateTime(2018, 6, 19, 13, 0, 0);
+        DateTime night = new DateTime(2018, 6, 19, 18, 0, 0);
+
+        //Message du week end par defaut
+        private String HelloMessage = "Bon Week-end " + Environment.UserName;
+
+        //Constructeur de la classe Message
+        public Message(DateTime maintenant)
+        {
+            //HelloMessage égale à Bonjour si un jour de la semaine de 9h à 13h
+            if (maintenant.Hour> morning.Hour && maintenant.Hour < afternoon.Hour && (int)maintenant.DayOfWeek >= 1 && (int)maintenant.DayOfWeek <= 5)
+            {
+                HelloMessage = "Bonjour " + Environment.UserName;
+            }
+            //HelloMessage égale à Von après-midi si un jour de la semaine de 13h à 18h
+            else if (maintenant.Hour > afternoon.Hour && maintenant.Hour < night.Hour && (int)maintenant.DayOfWeek >= 1 && (int)maintenant.DayOfWeek <= 5)
+            {
+                HelloMessage = "Bon après-midi " + Environment.UserName;
+            }
+            else if ( maintenant.Hour > night.Hour && maintenant.Hour < morning.Hour && (int)maintenant.DayOfWeek >= 1 && (int)maintenant.DayOfWeek <= 5)
+            //HelloMessage égale à Von après-midi si un jour de la semaine de 18h à 9h
+            {
+                HelloMessage = "Bonsoir " + Environment.UserName;
+            }
+        }
+
+        //Getter pour récupérer la variable String de HelloMessage dans la main
+        public String GetHelloMessage()
+        {
+            return this.HelloMessage;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            //Variables de test : Maintenant et une manuel
+            //DateTime today = DateTime.Now;
+            DateTime today = new DateTime(2019,6,24,10,0,0);
+
+            //Instansiation du message avec la date en paramètre
+            Message message = new Message(today);
+            //Utilisation du Getter pour récupérer le message
+            String helloMessage = message.GetHelloMessage();
+
+            //Valeur null par defaut
+            int exit = 0;
+
+            //Bouvle d'affichage du message
+            while (exit == 0)
+            {
+                //Affiche le message dans la console
+                Console.WriteLine(helloMessage);
+
+                //Le programme s'arrete si la touche est différente de Enter, sinon elle reboucle sur l'affichage du message
+                if(Console.ReadKey().Key != ConsoleKey.Enter) { exit = 1; }
+                else { exit = 0; }  
+            }
+        }
+    }
+}
